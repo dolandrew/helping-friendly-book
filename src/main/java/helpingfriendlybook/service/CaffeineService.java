da43487005e4f915e552b2caeb0106a96bbd833f
Andrew Dolan <andrew.dolan@skyward.io>
2021-09-23 09:33:06 -0700

    Add CaffeineSerivce to keep app awake

---
+++ src/main/java/helpingfriendlybook/service/CaffeineService.java
@@ -0,0 +1,25 @@
+package helpingfriendlybook.service;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+public class CaffeineService {
+
+    private static final Logger LOG = LoggerFactory.getLogger(CaffeineService.class);
+
+    private final RestTemplate restTemplate;
+
+    public CaffeineService(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
+
+    @Scheduled(initialDelay = 0, fixedDelay = 1000 * 60 * 15)
+    public void guayusa() {
+        LOG.warn("Caffeinating...");
+        restTemplate.getForObject("http://helping-friendly-book.herokuapp.com", Void.class);
+    }
+}
